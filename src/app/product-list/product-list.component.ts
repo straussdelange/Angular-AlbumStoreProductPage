6ecc7386965746d854d1ec29141b1d15f907fd4b
Strauss <straussdelange@gmail.com>
2020-10-15 12:06:03 +0200

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../product';
 
-import { Product Service} from '../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -11,9 +11,13 @@ import { Product Service} from '../product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response =>
+      this.products = response);
   }
 
 }
