4d1035d175b6ce27b97e83ee7d1b58be4fc75968
Strauss <straussdelange@gmail.com>
2020-10-15 12:27:33 +0200

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+const app Routes: Routes ();
 
 @NgModule({
   declarations: [
